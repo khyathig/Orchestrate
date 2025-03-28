491f4e5645ecd9897ee08866f7dbbb9b974a7428
Khushi Mittal <145904463+khushi8k3@users.noreply.github.com>
2025-03-28 12:39:17 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);  //  Correct server initialization
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",  //  Allow frontend to connect
-    origin: "https://orchestrate-sooty.vercel.app/",
+    origin: "https://orchestrate-five.vercel.app/",
     methods: ["GET", "POST"]
   }
 });
