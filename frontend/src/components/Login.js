5681db06f70aa33be837aa60c47f837c168bd6af
Khushi Mittal <145904463+khushi8k3@users.noreply.github.com>
2025-03-28 12:38:26 +0530

    Update Login.js

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -22,7 +22,7 @@ const Login = ({ setLoggedInUser }) => {
         setError("");
         
         try {
-            const res = await axios.post("http://localhost:5000/api/auth/login", {
+            const res = await axios.post("https://orchestrate-1.onrender.com/api/auth/login", {
                 email,
                 password,
             });
