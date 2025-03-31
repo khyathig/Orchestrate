43f25635ff9666a19d71a63f5a48b8ee37874333
khyathig <khyathigutta22@gmail.com>
2025-03-31 22:38:30 +0530

    Update Employee.js

--- backend/models/Employee.js
+++ backend/models/Employee.js
@@ -1,5 +1,14 @@
 const mongoose = require("mongoose");
 
+/**
+ * @typedef {Object} Employee
+ * @property {string} name - The name of the employee.
+ * @property {string} email - The unique email of the employee.
+ * @property {string} password - The hashed password of the employee.
+ * @property {string} [team="General"] - The team to which the employee belongs.
+ * @property {string} [role="employee"] - The role of the employee (default is "employee").
+ */
+
 const employeeSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
@@ -7,5 +16,10 @@ const employeeSchema = new mongoose.Schema({
     team: { type: String, default: "General" },
     role: { type: String, default: "employee" }
 });
+
+/**
+ * Mongoose model for Employee collection.
+ * @type {mongoose.Model<Employee>}
+ */
 
 module.exports = mongoose.model("Employee", employeeSchema);
